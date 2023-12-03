bacea96ef75a778eed7b9202d28fde142d9497b4
ussenuk <ukimanuka@gmail.com>
2023-12-03 14:31:25 +0300

    worked on event handling lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React from "react";
+
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+
+    function handleFocus(){
+        console.log("Good!")
+    }
+
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
